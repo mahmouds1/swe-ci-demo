01de1b4eff3e19596ed102b9972b214e54d9be30
Sarah <mahmouds@wit.edu>
2022-03-30 14:38:22 -0400

    added proper code

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -1,17 +1,29 @@
 package com.demo;
 
-public class FizzBuzz 
-{
-    String processNumber(int num)
-	{	
-    	// Copy your code here
-		return "";
+public class FizzBuzz {
+	
+    String processNumber(int num) {
+	    
+		if ( (num % 15 == 0) ) {
+		  return "FizzBuzz";
+		} else if (num % 3 == 0) {
+		  return "Fizz";
+		} else if (num % 5 == 0) {
+		  return "Buzz";
+		} else {
+		  return num + "";
+		}
+
 	}
 	
-	String processNumbers (int[] numList)
-	{
-    	// Copy your code here
-		return "";
+	String processNumbers (int[] numList) {
+		
+		int len = numList.length, i;
+		String finalRes = "";
+		for (i = 0; i < len-1; i++){
+		  finalRes += processNumber(numList[i]) + ", ";
+		}
+		return finalRes + processNumber(numList[len-1]);
 	}
 
 }
